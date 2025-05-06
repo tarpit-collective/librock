0bc16666d1d839e6a1b837696af98d3922298af4
Jacob Sinclair <jcbsnclr@outlook.com>
2025-05-06 22:41:23 +0100

    start work on impl

--- include/librock.h
+++ include/librock.h
@@ -10,48 +10,87 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// typedef struct {
-//     size_t refs;
-//     size_t len;
-//     char buf[];
-// } rk_ref_t;
-
-// typedef struct {
-//     rk_ref_t *back;
-//     size_t start, end;
-// } rk_str_t;
-
-typedef uint32_t rk_id_t;
-
-typedef struct rk_pool rk_pool_t;
+typedef int64_t rk_id_t;
 
 typedef struct {
-    rk_pool_t *pool;
-    rk_id_t id;
-} rk_str_t;
-
-typedef struct {
-    enum { RK_ERR_EOM } kind;
+    enum {
+        RK_OK,
+        RK_ERR_EOM,
+        RK_ERR_ID,
+    } code;
 } rk_err_t;
 
-// owns access to pool
-rk_str_t rk_str_new(rk_pool_t *pl, const char *data, size_t len, rk_err_t *err);
-rk_str_t rk_str_cat(rk_pool_t *pl, rk_str_t lhs, rk_str_t rhs, rk_err_t *err);
-rk_str_t rk_str_slice(rk_pool_t *pl, rk_str_t str, size_t start, size_t end, rk_err_t *err);
-rk_str_t rk_str_clone(rk_pool_t *pl, rk_str_t str);
+// vtable
+typedef struct rk_pool_vt rk_pool_vt_t;
 
-// TODO: head/tail takes first/last n elements.
+typedef rk_id_t (*rk_pool_new_t)(rk_pool_vt_t *pl, const void *dptr, size_t len, rk_err_t *err);
+typedef void (*rk_pool_del_t)(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
+typedef void (*rk_pool_resize_t)(rk_pool_vt_t *pl, rk_id_t id, size_t new_size, rk_err_t *err);
 
-// immutable view of pool
-const char *rk_str_data(rk_str_t str);
-size_t rk_str_len(rk_str_t str);
-bool rk_str_eq(rk_str_t lhs, rk_str_t rhs);
+typedef void *(*rk_pool_get_ptr_t)(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
+typedef size_t (*rk_pool_get_size_t)(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
 
-void rk_str_ref(rk_str_t str);
-void rk_str_unref(rk_str_t str);
+typedef void (*rk_pool_free_t)(rk_pool_vt_t *pl);
+
+const char *rk_magic_str;
+
+struct rk_pool_vt {
+    char magic[8];
+    rk_pool_new_t new;
+    rk_pool_del_t del;
+    rk_pool_resize_t resize;
+
+    rk_pool_get_ptr_t get_ptr;
+    rk_pool_get_size_t get_size;
+
+    rk_pool_free_t free;
+};
+
+// typedef struct {
+//     rk_pool_vt_t vt;
+//     char *data;
+//     size_t len, ptr;
+// } rk_arena_pool_t;
+
+#define RK_ID_MAX 16
+
+typedef struct {
+    rk_pool_vt_t vt;
+
+    struct {
+        void *ptr;
+        size_t size;
+    } *objs[RK_ID_MAX];
+} rk_sys_pool_t;
+
+#define RK_POOL(pl) (rk_pool_vt_t *)(pl)
+
+rk_id_t rk_pool_new(rk_pool_vt_t *pl, const void *dptr, size_t len, rk_err_t *err);
+void rk_pool_del(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
+void rk_pool_resize(rk_pool_vt_t *pl, rk_id_t id, size_t new_size, rk_err_t *err);
+
+void *rk_pool_get_ptr(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
+size_t rk_pool_get_size(rk_pool_vt_t *pl, rk_id_t id, rk_err_t *err);
+
+void rk_pool_free(rk_pool_vt_t *pl);
+
+rk_sys_pool_t rk_sys_pool(rk_err_t *err);
 
 // ****** IMPLEMENTATION ******
 #ifdef LIBROCK_IMPL
+
+// static rk_id_t rk_sys_pool_new(rk_pool_vt_t *pl, const void *dptr, size_t len, rk_err_t *err) {
+// }
+
+static const rk_pool_vt_t rk_sys_pool_vt = {
+
+};
+
+rk_sys_pool_t rk_sys_pool(rk_err_t *err) {
+    rk_sys_pool_t pl;
+
+    return pl;
+}
 
 #endif
 
--- tests/main.c
+++ tests/main.c
@@ -1,10 +1,38 @@
 #include <munit.h>
+#include <stdio.h>
 
 #define LIBROCK_IMPL
 #include <librock.h>
 
-static const MunitSuite test_suite = {0};
+MunitResult sys_pool_create_free(const MunitParameter params[], void *usr) {
+    (void)usr;
+    (void)params;
+
+    fprintf(stderr, "LMAOOOOO\n");
+    // munit_assert(1 == 0);
+
+    return MUNIT_OK;
+}
+
+static MunitTest tests[] = {
+    {"/sys_pool_create_free", sys_pool_create_free, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
+
+static const MunitSuite test_suite = {"/pools", tests, NULL, 1, MUNIT_SUITE_OPTION_NONE};
 
 int main(int argc, char *argv[]) {
+    // rk_err_t err;
+    // rk_arena_pool_t arena = rk_arena_pool(0x10000, &err);
+    // rk_pool_vt_t *pl = RK_POOL(&arena);
+
+    // const char *hello = "Hello, World!";
+    // rk_id_t msg = rk_pool_new(pl, hello, strlen(hello), &err);
+
+    // const char *ptr = rk_pool_get_ptr(pl, msg, &err);
+    // printf("message: %s\n", ptr);
+
+    // rk_pool_del(pl, msg, &err);
+    // rk_pool_free(pl);
+
     return munit_suite_main(&test_suite, (void *)"µnit", argc, argv);
 }

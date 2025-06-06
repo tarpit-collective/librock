6c15d5d966a7656ac81346e8bbe2e44fde2db5c2
Jack Clarke <jackojc@gmail.com>
2025-06-06 22:19:56 +0100

    Remove unused tests

---
+++ tests/fail.c
@@ -0,0 +1,7 @@
+#include <librock/librock.h>
+
+int main(int argc, const char* argv[]) {
+	RK_UNUSED(argc, argv);
+
+	return 1;
+}
--- tests/pass.c
+++ tests/pass.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-
 #include <librock/librock.h>
 
 int main(int argc, const char* argv[]) {
 	RK_UNUSED(argc, argv);
-
-	fprintf(stdout, "stdout\n");
-	fprintf(stderr, "stderr\n");
 
 	return 0;
 }

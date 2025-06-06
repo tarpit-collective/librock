06ab5f65720edd3136ae21474ad9ab4559352ad8
Jack Clarke <jackojc@gmail.com>
2025-06-06 01:38:18 +0100

    Add definitions to librock.h

--- tests/test_librock.c
+++ tests/test_librock.c
@@ -2,8 +2,11 @@
 
 #include <librock/librock.h>
 
-int main(int argc, const char *argv[]) {
-  fprintf(stdout, "foo\n");
-  fprintf(stderr, "bar\n");
-  return 0;
+int main(int argc, const char* argv[]) {
+	RK_UNUSED(argc, argv);
+
+	fprintf(stdout, "stdout\n");
+	fprintf(stderr, "stderr\n");
+
+	return 0;
 }
